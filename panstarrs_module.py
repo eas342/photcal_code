734c47ea1619c858ffaa071b795df714d1f22dc4
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-01-05 19:48:47 +0100

    I added a method to the clusterPhotometry object in the panstarrs_module to plot color magnitude diagrams.

--- panstarrs_module.py
+++ panstarrs_module.py
@@ -21,6 +21,36 @@ class clusterPhot(object):
             print('Warning, closest source is '+str(minimumDist*3600.)+' arcsec for'+str(ra)+' '+str(dec))
         else:
             return self.dat[rowIndex]
+    
+    def plot_cm(self,color1='g',color2='r',mag='g',ra='ra',dec='dec',racen=114.5958,deccen=21.573):
+        """ 
+        Plots a color-magnitude diagram from the photometry 
+        
+        Parameters
+        --------------
+        color1: str
+            First photometric band
+        color2: str
+            Second photometric band
+        mag: str
+            Magnitude photometric band
+        """
+        fig, ax = plt.subplots()
+        
+        deltaRA = racen - self.dat['ra']
+        deltaDEC = deccen - self.dat['dec']
+        deltaDistApprox = np.sqrt(deltaRA**2 / np.sin(self.dat['dec'])**2 + deltaDEC**2)
+        cpoints = deltaDistApprox < 5./60.
+        cdat = self.dat[cpoints]
+        
+        ax.plot(cdat[color1] - cdat[color2],cdat[mag],'.',rasterized=True)
+        ax.set_xlabel(color1+' - '+color2)
+        ax.set_ylabel(mag)
+        ax.invert_yaxis()
+        
+        self.ax = ax
+        self.fig = fig
+        #fig.show()
 
 if __name__ == "__main__":
     cP = clusterPhot()
