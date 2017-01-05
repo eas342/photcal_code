bb9dae0e99765c614dc409efa4090805190613cc
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-01-05 19:47:47 +0100

    I added the multi_module script to work with multiple modules.

---
+++ multi_module.py
@@ -0,0 +1,20 @@
+from astropy.io import ascii
+from astropy.table import Table
+import numpy as np
+import hecto_module as hm
+import panstarrs_module as ps
+import mk_module as mk
+import pdb
+import os
+
+def do_cm():
+    """
+    Does a color magnitude diagram
+    """
+    psObj = ps.clusterPhot()
+    psObj.plot_cm()
+    mkObj = mk.clusterClassification()
+    mkObj.get_phot()
+    psObj.ax.plot(mkObj.photdat['Color (g-r)'],mkObj.photdat['g'],'o',color='red')
+    psObj.fig.savefig('colormag.pdf')
+    #psObj.fig.show()
