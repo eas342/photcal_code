c61e109268095e64babd973776485868365aa3a9
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-01-08 23:03:52 -0700

    multi_module.do_cm() can now also do the field of view plot with NIRCam footprint.

--- multi_module.py
+++ multi_module.py
@@ -14,12 +14,16 @@ def make_cluster_file():
     dat = pd.read_excel('data/cluster_data.xlsx')
     dat.to_csv('data/cluster_data.csv',index=False)
 
-def do_cm():
+def do_cm(fov=False):
     """
     Does a color magnitude diagram
     """
     psObj = ps.clusterPhot()
-    psObj.plot_cm()
+    if fov == True:
+        psObj.plot_fov()
+    else:
+        psObj.plot_cm()
+        
     thisCluster = 'NGC 2420'
     cDat = ascii.read('data/cluster_data.csv')
     rowLook = cDat['Name'] == thisCluster
@@ -30,11 +34,26 @@ def do_cm():
     colorArr = ['red','orange','green']
     for oneType,dispCol in zip(typeExplore,colorArr):
         mkObj.get_phot(sType=oneType)
-        psObj.ax.plot(mkObj.photdat['Color (g-r)'],mkObj.photdat['g'],'o',color=dispCol,
-                      label=oneType)
+        if fov == True:
+            psObj.ax.plot(mkObj.photdat['ra'],mkObj.photdat['dec'],'o',color=dispCol,
+                          label=oneType)
+        else:
+            psObj.ax.plot(mkObj.photdat['Color (g-r)'],mkObj.photdat['g'],'o',color=dispCol,
+                          label=oneType)
+    if fov == False:
+        psObj.ax.axvline(x=colorShow,linewidth=7.,alpha=0.3,color='red')
+        
+    psObj.ax.legend(loc='best',frameon=True)
     
-    psObj.ax.axvline(x=colorShow,linewidth=7.,alpha=0.3,color='red')
-    psObj.ax.legend(loc='best')
-    
-    psObj.fig.savefig('plots/colormag.pdf')
+    if fov == True:
+        psObj.fig.savefig('plots/fov.pdf')
+    else:
+        psObj.fig.savefig('plots/colormag.pdf')
     #psObj.fig.show()
+    
+def do_fov():
+    """
+    Shows the location of stars in the field 
+    """
+    do_cm(fov=True)
+    
