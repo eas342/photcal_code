e03e9a1e0a1b17ee52d6e2c80c7dc0a878802fbe
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-03-22 00:28:05 -0700

    I adjusted hecto_module.prepForMK() to do some robust polynomial spectra rectification.

--- hecto_module.py
+++ hecto_module.py
@@ -4,11 +4,16 @@ from astropy.table import Table
 import es_gen
 from astropy.convolution import Gaussian1DKernel, convolve
 import numpy as np
+import yaml
+import multi_module
+import matplotlib.pyplot as plt
+import pdb
 
 class clusterSpec(object):
-    def __init__(self,kernelWidth=2.5,
-                redFile='../hectospec_data/2016.0207/reduction/0102/spHect-ngc2420_cat_1.2468-0102.fits'):
-        hdulist = fits.open(redFile)
+    def __init__(self,kernelWidth=2.5,src='NGC 2420'):
+        self.src = src
+        self.redFile = multi_module.getRedFile(src,dataType='hectoData')
+        hdulist = fits.open(self.redFile)
         self.fibinfo = Table(hdulist[5].data)
         self.makeCleanNames()
         self.goodfib = np.array(es_gen.es_strmatch('O-*',self.fibinfo['OBJTYPE']))
@@ -16,6 +21,7 @@ class clusterSpec(object):
         self.flux2D = hdulist[1].data
         self.fluxErr2D = 1/np.sqrt(hdulist[2].data)
         self.kernelWidth=kernelWidth
+    
     
     def getbyObjName(self,objName):
         """ Search by object name
@@ -41,7 +47,7 @@ class clusterSpec(object):
             cleanNames.append(newString)
         self.fibinfo['cleanName'] = cleanNames
         
-    def prepForMK(self):
+    def prepForMK(self,showFig=False):
         """
         Prepares the spectra for MKClass
         """
@@ -51,9 +57,34 @@ class clusterSpec(object):
             x = self.wave2D[currentFib,:]
             y = self.flux2D[currentFib,:]
             yconv = convolve(y, datkern, boundary='extend')
-            ynorm = yconv / np.median(yconv)
+            
+            ## Points with useful features for classification by mkClass
             keepPts = (x > 3800) & (x < 5600)
             outX = x[keepPts]
+            trimY = yconv[keepPts]
+            
+            ## Do a fit in log space so it won't give you zeros
+            yLog = np.log10(trimY)
+            roughPoly = es_gen.robust_poly(outX,yLog,8,sigreject=8.)
+            modelF1 = 10**np.polyval(roughPoly,outX)
+            ynorm1 = trimY / modelF1
+            
+            roughPoly2 = es_gen.robust_poly(outX,ynorm1,10,sigreject=15.)
+            modelF2 = np.polyval(roughPoly2,outX)
+            ynorm2 = ynorm1 / modelF2
+            
+            if showFig == True:
+                plt.close('all')
+                fig, (ax1, ax2, ax3) = plt.subplots(3)
+                ax1.plot(outX,trimY)
+                ax1.plot(outX,modelF1)
+                ax2.plot(outX,ynorm1)
+                ax2.plot(outX,modelF2)
+                ax3.plot(outX,ynorm2)
+                
+                fig.show()
+            pdb.set_trace()
+
             #CorrectionFunc = np.interp(outX,xinterp,ymod)
             outY = ynorm[keepPts]
             #outY = ynorm[keepPts] / CorrectionFunc
